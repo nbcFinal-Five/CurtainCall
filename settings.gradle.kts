d4b3a5ce75ad30e968859a8e97a4d3d6b295e4ba
LEE HO <dlho1222@naver.com>
2024-02-25 21:05:49 +0900

    #5[feat]kopis api 연결

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri ("http://oss.sonatype.org/content/repositories/snapshots")
+            isAllowInsecureProtocol = true
+        }
     }
 }
 
