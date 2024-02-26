29648b7505cbb4aab1fa7e443926ce339e20b518
kwj9002@gmail.com <150776867+kwj9002@users.noreply.github.com>
2024-02-26 11:36:30 +0900

    feat: 카카오 api 키 추가, DetailFragment mapView 추가 ui: id 수정, 장르 shape 추가, 리미티드런 shape 추가

--- app/src/main/java/com/nbc/curtaincall/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/nbc/curtaincall/ui/detail/DetailFragment.kt
@@ -5,18 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapView
 import com.nbc.curtaincall.R
 import com.nbc.curtaincall.databinding.FragmentDetailBinding
-import com.nbc.curtaincall.databinding.FragmentHomeBinding
-import com.nbc.curtaincall.ui.home.HomeViewModel
 
 class DetailFragment : Fragment() {
 
     private var _binding: FragmentDetailBinding? = null
-
     private val binding get() = _binding!!
+
+    private lateinit var mapView: MapView
+
+    private val viewModel: DetailViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +29,22 @@ class DetailFragment : Fragment() {
     ): View {
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        mapView = root.findViewById(R.id.map_view)
+        mapView.start(object : MapLifeCycleCallback() {
+            override fun onMapDestroy() {
+                // 지도 API 가 정상적으로 종료될 때 호출됨
+            }
+
+            override fun onMapError(error: Exception) {
+                // 인증 실패 및 지도 사용 중 에러가 발생할 때 호출됨
+            }
+        }, object : KakaoMapReadyCallback() {
+            override fun onMapReady(kakaoMap: KakaoMap) {
+                // 인증 후 API 가 정상적으로 실행될 때 호출됨
+            }
+        })
+
         return root
     }
 
