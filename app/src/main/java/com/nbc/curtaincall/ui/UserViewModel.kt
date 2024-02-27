43923bed05df4b0e210c4b90e0472b81b1d3f9ed
kkukileon305 <kkukileon305@gmail.com>
2024-02-27 21:02:24 +0900

    [feat] 닉네임 중복검사 추가

--- app/src/main/java/com/nbc/curtaincall/ui/UserViewModel.kt
+++ app/src/main/java/com/nbc/curtaincall/ui/UserViewModel.kt
@@ -4,14 +4,12 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.nbc.curtaincall.BuildConfig
 import com.nbc.curtaincall.Supabase
-import io.github.jan.supabase.createSupabaseClient
-import io.github.jan.supabase.gotrue.Auth
+import io.github.jan.supabase.exceptions.BadRequestRestException
+import io.github.jan.supabase.exceptions.RestException
 import io.github.jan.supabase.gotrue.auth
 import io.github.jan.supabase.gotrue.providers.builtin.Email
 import io.github.jan.supabase.gotrue.user.UserInfo
-import io.github.jan.supabase.postgrest.Postgrest
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -37,6 +35,9 @@ class UserViewModel : ViewModel() {
 	private val _signUpResult = MutableLiveData<Boolean?>(null)
 	val signUpResult: LiveData<Boolean?> get() = _signUpResult
 
+	private var _signUpErrorMessage = MutableLiveData<String?>(null)
+	val signUpErrorMessage: LiveData<String?> = _signUpErrorMessage
+
 	fun signIn(inputEmail: String, inputPassword: String) {
 		_isSignInLoading.value = true
 
@@ -51,8 +52,8 @@ class UserViewModel : ViewModel() {
 					_userInfo.value = Supabase.client.auth.currentUserOrNull()
 					_signInResult.value = true
 				}
-			} catch (e: Exception) {
-				Log.d("sign in", e.toString())
+			} catch (e: RestException) {
+				Log.d("sign in", e.error)
 				withContext(Dispatchers.Main) {
 					_signInResult.value = false
 				}
@@ -68,13 +69,14 @@ class UserViewModel : ViewModel() {
 		CoroutineScope(Dispatchers.IO).launch {
 			try {
 				Supabase.client.auth.signOut()
-			} catch (e: Exception) {
-				Log.d("sign out", e.toString())
+			} catch (e: RestException) {
+				Log.d("sign out", e.error)
 			} finally {
 				withContext(Dispatchers.Main) {
 					_userInfo.value = null
 					_signInResult.value = null
 					_signUpResult.value = null
+					_signUpErrorMessage.value = null
 				}
 			}
 		}
@@ -104,12 +106,13 @@ class UserViewModel : ViewModel() {
 				withContext(Dispatchers.Main) {
 					_userInfo.value = Supabase.client.auth.currentUserOrNull()
 					_signUpResult.value = true
+					_signUpErrorMessage.value = null
 				}
-			} catch (e: Exception) {
-				Log.d("sign up", e.toString())
-
+			} catch (e: RestException) {
+				Log.d("sign up", e.error)
 				withContext(Dispatchers.Main) {
 					_signUpResult.value = false
+					_signUpErrorMessage.value = e.error
 				}
 			} finally {
 				withContext(Dispatchers.Main) {
