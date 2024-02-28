6d00fc47b9edc7572e00533125e61db1433ff9b8
kkukileon305 <kkukileon305@gmail.com>
2024-02-28 19:28:28 +0900

    [feat] 내정보 리뷰리스트 불러오기 구현

--- app/src/main/java/com/nbc/curtaincall/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/nbc/curtaincall/ui/mypage/MyPageFragment.kt
@@ -9,12 +9,14 @@ import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.nbc.curtaincall.R
 import com.nbc.curtaincall.ui.auth.AuthActivity
 import com.nbc.curtaincall.databinding.FragmentMyPageBinding
 import com.nbc.curtaincall.ui.UserViewModel
 
 class MyPageFragment : Fragment() {
 	private val userViewModel by lazy { ViewModelProvider(this)[UserViewModel::class.java] }
+	private val myPageViewModel by lazy { ViewModelProvider(this)[MyPageViewModel::class.java] }
 
 	private var _binding: FragmentMyPageBinding? = null
 	private val binding get() = _binding!!
@@ -58,13 +60,35 @@ class MyPageFragment : Fragment() {
 		userViewModel.userInfo.observe(viewLifecycleOwner) {
 			if (it == null) {
 				binding.clUserInfo.visibility = View.INVISIBLE
-				binding.btnOpenAuthActivity.visibility = View.VISIBLE
+				binding.cvOpenAuthActivity.visibility = View.VISIBLE
+				binding.tvNickname.text = ""
+				binding.tvEmail.text = ""
+				binding.tvSignOut.visibility = View.INVISIBLE
+
+				myPageViewModel.clearReviews()
 			} else {
 				binding.clUserInfo.visibility = View.VISIBLE
-				binding.btnOpenAuthActivity.visibility = View.INVISIBLE
-				binding.tvUserInfoEmail.text = it.userMetadata.toString()
+				binding.cvOpenAuthActivity.visibility = View.INVISIBLE
+				binding.tvNickname.text = it.userMetadata!!["name"].toString().removeSurrounding("\"")
+				binding.tvEmail.text = it.email
+				binding.tvSignOut.visibility = View.VISIBLE
+
+				myPageViewModel.setReview(it.id)
 			}
 		}
+
+		// reviews
+		myPageViewModel.reviews.observe(viewLifecycleOwner) {
+			if (it != null) {
+				binding.tvReviewCount.text = it.size.toString() + getString(R.string.count)
+			}
+		}
+
+		myPageViewModel.isReviewLoading.observe(viewLifecycleOwner) {
+			if (it) binding.tvReviewCount.text = "불러오는 중"
+		}
+
+		// bookmarks
 	}
 
 	override fun onDestroyView() {
--- app/src/main/java/com/nbc/curtaincall/ui/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/nbc/curtaincall/ui/mypage/MyPageViewModel.kt
@@ -1,13 +1,62 @@
 package com.nbc.curtaincall.ui.mypage
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.nbc.curtaincall.supabase.Supabase
+import com.nbc.curtaincall.supabase.model.GetReviewModel
+import io.github.jan.supabase.exceptions.RestException
+import io.github.jan.supabase.postgrest.from
+import io.github.jan.supabase.postgrest.query.Columns
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MyPageViewModel : ViewModel() {
+	private val _isReviewLoading = MutableLiveData(false)
+	val isReviewLoading: LiveData<Boolean>
+		get() = _isReviewLoading
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
-    }
-    val text: LiveData<String> = _text
+	private val _reviews = MutableLiveData<List<GetReviewModel>?>(null)
+	val reviews: LiveData<List<GetReviewModel>?>
+		get() = _reviews
+
+	fun setReview(userId: String) {
+		_isReviewLoading.value = true
+
+		CoroutineScope(Dispatchers.IO).launch {
+			try {
+				val columns = Columns.raw(
+					"""
+					*, profile:profiles(*)
+				""".trimIndent()
+				)
+
+				val reviews = Supabase.client
+					.from("reviews")
+					.select(columns) {
+						filter {
+							eq("user_id", userId)
+						}
+					}
+					.decodeList<GetReviewModel>()
+				
+				withContext(Dispatchers.Main) {
+					_reviews.value = reviews
+				}
+			} catch (e: RestException) {
+				Log.d("my reviews", e.error)
+			} finally {
+				withContext(Dispatchers.Main) {
+					_isReviewLoading.value = false
+				}
+			}
+		}
+	}
+
+	fun clearReviews() {
+		_reviews.value = null
+	}
 }
