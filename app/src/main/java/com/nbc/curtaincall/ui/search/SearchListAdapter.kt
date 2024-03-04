83dd260ce59980a4aa3070cc6e3b3eed500bcc46
CodeNewbieee <choi2232@hanmail.net>
2024-03-04 11:14:09 +0900

    [feat] 검색화면에서 검색결과 포스터 클릭시 해당 정보를 가진 간단화면 호출

--- app/src/main/java/com/nbc/curtaincall/ui/search/SearchListAdapter.kt
+++ app/src/main/java/com/nbc/curtaincall/ui/search/SearchListAdapter.kt
@@ -5,26 +5,23 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.nbc.curtaincall.search.model.SearchItem
+import coil.load
 import com.nbc.curtaincall.databinding.ConstraintLayoutSearchRvBinding
+import com.nbc.curtaincall.search.model.SearchItem
+import com.nbc.curtaincall.ui.home.adapter.PosterClickListener
 
-class SearchListAdapter : ListAdapter<SearchItem, SearchListAdapter.SearchShowViewHolder>(searchCallback) {
-    interface ItemCLick {
-        fun onClick (position: Int)
-    }
-    var itemClick : ItemCLick? = null
+class SearchListAdapter(private val listener: PosterClickListener? = null) :
+    ListAdapter<SearchItem, SearchListAdapter.SearchShowViewHolder>(searchCallback) {
 
     companion object {
         private val searchCallback = object : DiffUtil.ItemCallback<SearchItem>() {
             override fun areItemsTheSame(oldItem: SearchItem, newItem: SearchItem): Boolean {
-                return oldItem.hashCode() == newItem.hashCode()
+                return oldItem.mt20id == newItem.mt20id
             }
 
             override fun areContentsTheSame(oldItem: SearchItem, newItem: SearchItem): Boolean {
                 return oldItem == newItem
             }
-
         }
     }
 
@@ -35,17 +32,20 @@ class SearchListAdapter : ListAdapter<SearchItem, SearchListAdapter.SearchShowVi
     override fun onBindViewHolder(holder: SearchShowViewHolder, position: Int) {
         val showItem = getItem(position)
         holder.bind(showItem)
-        holder.itemView.setOnClickListener {
-            itemClick?.onClick(position)
-        }
     }
 
     inner class  SearchShowViewHolder(private val binding:ConstraintLayoutSearchRvBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item : SearchItem) {
             with(binding) {
-                Glide.with(itemView).load(item.poster).into(ivSearchResult)
+//                Glide.with(itemView).load(item.poster).into(ivSearchResult)
                 tvSearchitemGenre.setText(item.genrenm)
                 tvSearchitemPrfstate.setText(item.prfstate)
+                ivSearchResult.load(item.poster)
+                ivSearchResult.setOnClickListener {
+                    item.mt20id?.let { id ->
+                        listener?.posterClicked(id) // 클릭시 해당 id 전달하여 간단화면 띄우기
+                    }
+                }
             }
         }
     }
