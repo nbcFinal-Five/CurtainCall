8d1e5686042176c3086dbd086fd0ae6444465377
CodeNewbieee <choi2232@hanmail.net>
2024-03-04 11:20:30 +0900

    [refactor] Glide 라이브러리 재추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,6 +104,10 @@ dependencies {
 	//BottomSheet, Chip
 	implementation("com.google.android.material:material:1.11.0")
 
+	//Glide
+	implementation("com.github.bumptech.glide:glide:4.16.0")
+	annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
 	// shimmer
 	implementation("com.facebook.shimmer:shimmer:0.5.0")
 }
--- app/src/main/java/com/nbc/curtaincall/ui/mypage/BookmarkListAdapter.kt
+++ app/src/main/java/com/nbc/curtaincall/ui/mypage/BookmarkListAdapter.kt
@@ -1,6 +1,5 @@
 package com.nbc.curtaincall.ui.mypage
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
