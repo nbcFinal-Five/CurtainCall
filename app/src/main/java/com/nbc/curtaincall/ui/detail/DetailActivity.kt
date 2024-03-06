e787c646faaafa397106c3d35b5a6775ad31ca6a
kkukileon305 <kkukileon305@gmail.com>
2024-03-06 23:23:03 +0900

    [feat] 상세의 기대평, 리뷰 버튼 생성

--- app/src/main/java/com/nbc/curtaincall/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/nbc/curtaincall/ui/detail/DetailActivity.kt
@@ -16,57 +16,57 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class DetailActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityDetailBinding
+	private lateinit var binding: ActivityDetailBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binding = ActivityDetailBinding.inflate(layoutInflater)
+		setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+		ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+			val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+			insets
+		}
 
-        val id = intent.getStringExtra(Constants.SHOW_ID)
-        if (id != null) {
-            fetchShowDetail(id)
-        }
-    }
+		val id = intent.getStringExtra(Constants.SHOW_ID)
+		if (id != null) {
+			fetchShowDetail(id)
+		}
+	}
 
-    private fun fetchShowDetail(id: String) {
-        lifecycleScope.launch {
-            runCatching {
-                withContext(Dispatchers.IO) {
-                    RetrofitClient.fetch.fetchShowDetail(path = id)
-                }
-            }.onSuccess { showDetail ->
-                showDetail.showList?.firstOrNull()?.let { firstShowDetail ->
-                    with(binding) {
-                        ivDetailPoster.load(firstShowDetail.poster)
-                        tvDetailShowNameSub.text = firstShowDetail.prfnm
-                        tvDetailGenreSub.text = firstShowDetail.genrenm
-                        tvDetailAgeSub.text = firstShowDetail.prfage
-                        tvDetailRuntimeSub.text = firstShowDetail.prfruntime
-                        tvDetailPriceSub.text = firstShowDetail.pcseguidance
-                        tvDetailShowStateSub.text = firstShowDetail.prfstate
-                        tvDetailPlaceSub.text = firstShowDetail.fcltynm
-                        tvDetailPeriodSub.text =
-                            "${firstShowDetail.prfpdfrom} ~ ${firstShowDetail.prfpdto}"
-                        tvDetailTimeSub.text = firstShowDetail.dtguidance
-                        tvDetailCastSub.text =
-                            if (firstShowDetail.prfcast.isNullOrBlank()) "미상" else firstShowDetail.prfcast
-                        tvDetailProductSub.text =
-                            if (firstShowDetail.entrpsnm.isNullOrBlank()) "미상" else firstShowDetail.entrpsnm
-                        ivDetailIntroPoster.load(firstShowDetail.styurls?.styurlList?.get(0))
-                        Log.d("TAG", "fetchShowDetail: ${firstShowDetail}")
-                    }
-                }
-            }.onFailure {
-                Toast.makeText(applicationContext, "통신 에러: ${it.message}", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        }
-    }
+	private fun fetchShowDetail(id: String) {
+		lifecycleScope.launch {
+			runCatching {
+				withContext(Dispatchers.IO) {
+					RetrofitClient.fetch.fetchShowDetail(path = id)
+				}
+			}.onSuccess { showDetail ->
+				showDetail.showList?.firstOrNull()?.let { firstShowDetail ->
+					with(binding) {
+						ivDetailPoster.load(firstShowDetail.poster)
+						tvDetailShowNameSub.text = firstShowDetail.prfnm
+						tvDetailGenreSub.text = firstShowDetail.genrenm
+						tvDetailAgeSub.text = firstShowDetail.prfage
+						tvDetailRuntimeSub.text = firstShowDetail.prfruntime
+						tvDetailPriceSub.text = firstShowDetail.pcseguidance
+						tvDetailShowStateSub.text = firstShowDetail.prfstate
+						tvDetailPlaceSub.text = firstShowDetail.fcltynm
+						tvDetailPeriodSub.text =
+							"${firstShowDetail.prfpdfrom} ~ ${firstShowDetail.prfpdto}"
+						tvDetailTimeSub.text = firstShowDetail.dtguidance
+						tvDetailCastSub.text =
+							if (firstShowDetail.prfcast.isNullOrBlank()) "미상" else firstShowDetail.prfcast
+						tvDetailProductSub.text =
+							if (firstShowDetail.entrpsnm.isNullOrBlank()) "미상" else firstShowDetail.entrpsnm
+						ivDetailIntroPoster.load(firstShowDetail.styurls?.styurlList?.get(0))
+						Log.d("TAG", "fetchShowDetail: ${firstShowDetail}")
+					}
+				}
+			}.onFailure {
+				Toast.makeText(applicationContext, "통신 에러: ${it.message}", Toast.LENGTH_SHORT)
+					.show()
+			}
+		}
+	}
 }
