8b56b6082c8a8e44ebf208f9796c5cd877d25875
CodeNewbieee <choi2232@hanmail.net>
2024-03-12 21:36:01 +0900

    [fix] 뷰페이저 가로 스크롤 과 네이버맵 가로스크롤 중첩 문제 해결

--- app/src/main/java/com/nbc/shownect/presentation/detail_activity/location/LocationFragment.kt
+++ app/src/main/java/com/nbc/shownect/presentation/detail_activity/location/LocationFragment.kt
@@ -1,20 +1,19 @@
 package com.nbc.shownect.ui.detail_activity.location
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.geometry.LatLngBounds
 import com.naver.maps.map.CameraPosition
-import com.naver.maps.map.MapFragment
+import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.NaverMapSdk
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
-import com.nbc.shownect.R
 import com.nbc.shownect.databinding.FragmentDetailLocationBinding
 import com.nbc.shownect.ui.detail_activity.DetailViewModel
 import com.nbc.shownect.util.Constants
@@ -25,17 +24,12 @@ class LocationFragment : Fragment(), OnMapReadyCallback {
     private var _binding: FragmentDetailLocationBinding? = null
     private val binding get() = _binding!!
     private val viewModel: DetailViewModel by activityViewModels<DetailViewModel>()
+    private lateinit var mapView: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //클라이언트 ID 지정
         NaverMapSdk.getInstance(requireContext()).client = NaverMapSdk.NaverCloudPlatformClient(Constants.MAP_CLIENT_ID)
-
-        val fm = childFragmentManager
-        val mapFragment = fm.findFragmentById(R.id.fv_hall_naver_map) as MapFragment?
-            ?: MapFragment.newInstance().also {
-            fm.beginTransaction().add(R.id.fv_hall_naver_map, it).commit()
-        }
-        mapFragment.getMapAsync(this)
     }
 
     override fun onCreateView(
@@ -45,13 +39,15 @@ class LocationFragment : Fragment(), OnMapReadyCallback {
     ): View? {
         _binding = FragmentDetailLocationBinding.inflate(inflater,container,false)
         setUpObserve()
+        mapView = binding.fvHallNaverMap
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.fetchDetailLocation()
-
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
     }
 
     private fun setUpObserve() {
@@ -86,7 +82,6 @@ class LocationFragment : Fragment(), OnMapReadyCallback {
 
                 // 지도 범위 제한 (대한민국)
                 naverMap.minZoom = 6.0
-                naverMap.maxZoom = 18.0
                 naverMap.extent = LatLngBounds(LatLng(32.973077, 124.270981), LatLng(38.856197,130.051725 ))
 
                 // 줌인 했을 때 내부 지도 있을 경우 띄우기
@@ -94,10 +89,39 @@ class LocationFragment : Fragment(), OnMapReadyCallback {
             }
         }
     }
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        mapView.onDestroy()
         _binding = null
     }
 
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
 }
