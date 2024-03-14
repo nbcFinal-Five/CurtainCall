cc3beee94a87189085d8a0550c821eb53c5c7a81
CodeNewbieee <choi2232@hanmail.net>
2024-03-14 12:33:22 +0900

    #107 [feat] 네트워크 연결 확인
    1. 네트워크 연결 상태 확인
    2. 미연결시 다이얼로그 띄위기
    3. 확인 버튼 클릭시 와이파이 연결 창 이동

--- app/src/main/java/com/nbc/curtaincall/presentation/main/MainActivity.kt
+++ app/src/main/java/com/nbc/curtaincall/presentation/main/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.nbc.curtaincall.ui.main
 
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -29,6 +35,35 @@ class MainActivity : AppCompatActivity() {
 		navView.setupWithNavController(navController)
 
 		initSupabase()
+
+		if(!isNetworkAvailable()){
+			showNetworkDialog()
+		}
+	}
+
+	// 네트워크 연결 상태 확인
+	private fun isNetworkAvailable(): Boolean {
+		val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+		val network = connectivityManager.activeNetwork
+		val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+		return networkCapabilities != null &&
+				(networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+						networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
+	}
+
+	// 네트워크 미연결 시 다이얼로그 창
+	private fun showNetworkDialog() {
+		val builder = AlertDialog.Builder(this,R.style.MyAlertDialogStyle)
+		builder.setTitle(R.string.network_check_necessary)
+			.setMessage(R.string.network_check_necessary_message)
+			.setCancelable(false)
+			.setPositiveButton(R.string.network_check) {
+			 	dialog, _ ->
+				dialog.dismiss()
+				startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))
+			}
+		val dialog = builder.create()
+		dialog.show()
 	}
 
 	private fun initSupabase(): SupabaseClient {
