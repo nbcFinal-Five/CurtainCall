6e82f40e05eb123adf8671734e4a8fabf7702aaa
kkukileon305 <kkukileon305@gmail.com>
2024-03-15 17:50:55 +0900

    유효검사 10글자 이상 제거,에러문구수정, 기대평 더보기 클릭시 이동

--- app/src/main/java/com/nbc/curtaincall/presentation/detail_activity/DetailActivity.kt
+++ app/src/main/java/com/nbc/curtaincall/presentation/detail_activity/DetailActivity.kt
@@ -11,39 +11,49 @@ import com.nbc.curtaincall.ui.detail_activity.adapter.DetailPagerAdapter
 import com.nbc.curtaincall.util.Constants
 
 class DetailActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityDetailBinding
-    private val detailViewModel by lazy { ViewModelProvider(this)[DetailViewModel::class.java] }
-    private val adapter: DetailPagerAdapter by lazy { DetailPagerAdapter(this) }
+	private lateinit var binding: ActivityDetailBinding
+	private val detailViewModel by lazy { ViewModelProvider(this)[DetailViewModel::class.java] }
+	private val adapter: DetailPagerAdapter by lazy { DetailPagerAdapter(this) }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        //뷰 페이저 탭 연결
-        with(binding) {
-            detailViewPager.adapter = adapter
-            TabLayoutMediator(tabLayoutDetail, detailViewPager) { tab, position ->
-                detailViewPager.currentItem = tab.position
-                tab.text = when (position) {
-                    0 -> "상세 정보"
-                    1 -> "소개 이미지"
-                    2 -> "공연장 위치"
-                    3 -> "기대평/리뷰"
-                    else -> ""
-                }
-            }.attach()
-        }
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binding = ActivityDetailBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+		//뷰 페이저 탭 연결
+		with(binding) {
+			detailViewPager.adapter = adapter
+			TabLayoutMediator(tabLayoutDetail, detailViewPager) { tab, position ->
+				detailViewPager.currentItem = tab.position
+				tab.text = when (position) {
+					0 -> "상세 정보"
+					1 -> "소개 이미지"
+					2 -> "공연장 위치"
+					3 -> "기대평/리뷰"
+					else -> ""
+				}
+			}.attach()
 
-        //TicketFragment에서 공연 id,공연장 id 받아 옴
-        val showId = intent.getStringExtra(Constants.SHOW_ID)
-        val facilityId = intent.getStringExtra(Constants.FACILITY_ID)
-        if (showId != null && facilityId != null) {
-            detailViewModel.sharedId(showId, facilityId)
-        }
-    }
+			val direct = intent.getBooleanExtra(Constants.IS_DIRECT, false)
+			if (direct) {
+				detailViewPager.currentItem = 3
+			}
+		}
+
+		ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+			val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+			insets
+		}
+
+		//TicketFragment에서 공연 id,공연장 id 받아 옴
+		val showId = intent.getStringExtra(Constants.SHOW_ID)
+		val facilityId = intent.getStringExtra(Constants.FACILITY_ID)
+		if (showId != null && facilityId != null) {
+			detailViewModel.sharedId(showId, facilityId)
+		}
+	}
+
+	fun changeFragER() {
+		binding.detailViewPager.currentItem = 3
+	}
 }
