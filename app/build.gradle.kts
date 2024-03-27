aebceaa84469e12fb8fda7e8ae9007a714af0fc6
LEE HO <dlho1222@naver.com>
2024-03-27 11:22:20 +0900

    release1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 		applicationId = "com.nbc.curtaincall"
 		minSdk = 26
 		targetSdk = 34
-		versionCode = 4
+		versionCode = 6
 		versionName = "1.2"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
