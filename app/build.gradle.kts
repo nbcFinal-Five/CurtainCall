82c61546985281df80ee62a6c470eb118ad2289c
LEE HO <dlho1222@naver.com>
2024-09-27 20:17:44 +0900

    version1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.nbc.curtaincall"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.5"
+        versionCode = 11
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "KOPIS_API_KEY", getAuthKey("KOPIS_API_KEY"))
